affcf37e393e698ba7db6cfeaf7c50b4a1f1b022
ChampKatika <inform.katika@gmail.com>
2021-11-12 14:50:57 +0700

    update evaluate

--- v1_evalulate.py
+++ v1_evalulate.py
@@ -8,14 +8,17 @@ user_counts = {
     'wisdm': 45,
 }
 
+model_name = 'lstm' # convlstm
+
 if __name__ == '__main__':
     for dataset in ['realWorld','shoaib', 'mHealth']:
         result = {
             'pLabel': [],
             'tLabel': [],
         }
+      
         for fold in range(1, user_counts[dataset]+1):
-            data = pd.read_csv(f"./raw_results/{dataset}_{fold}.csv")
+            data = pd.read_csv(f"./raw_results/{dataset}_{model_name}_{fold}.csv")
             result['pLabel'].extend(data['pLabel'].tolist())
             result['tLabel'].extend(data['tLabel'].tolist())
         print(metrics.confusion_matrix(result['tLabel'], result['pLabel']))
